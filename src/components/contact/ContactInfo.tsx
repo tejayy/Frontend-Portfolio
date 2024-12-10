ecd2da78d64c4b183ecad372995097573f8a652a
Tejas Jaybhaye <117623822+tejayy@users.noreply.github.com>
2024-12-10 19:29:53 +0530

    Update ContactInfo.tsx

--- src/components/contact/ContactInfo.tsx
+++ src/components/contact/ContactInfo.tsx
@@ -8,15 +8,15 @@ const ContactInfo = () => (
     <div className="space-y-4">
       <div className="flex items-center">
         <Mail className="w-6 h-6 text-blue-600 dark:text-blue-400 mr-4" />
-        <span className="dark:text-gray-300">contact@example.com</span>
+        <span className="dark:text-gray-300">tejas.jay07@gmail.com</span>
       </div>
       <div className="flex items-center">
         <Phone className="w-6 h-6 text-blue-600 dark:text-blue-400 mr-4" />
-        <span className="dark:text-gray-300">+1 (555) 123-4567</span>
+        <span className="dark:text-gray-300">+91 8237875792</span>
       </div>
       <div className="flex items-center">
         <MapPin className="w-6 h-6 text-blue-600 dark:text-blue-400 mr-4" />
-        <span className="dark:text-gray-300">San Francisco, CA</span>
+        <span className="dark:text-gray-300">Pune, Maharastra, INDIA</span>
       </div>
     </div>
   </div>
